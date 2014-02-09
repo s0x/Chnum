188a3550759539de26cbf777cc0afd2f88fed9f7
Ivo Senner <s0x@sixserv.org>
2014-02-10 00:33:48 +0100

    Install specific packages rather then just all
    Initialisation of a new environment has got its own function

--- sys/include/setup.sh
+++ sys/include/setup.sh
@@ -41,116 +41,117 @@ fn_exists() {
 }
 
 setup-env () {
-  log_info "Seting-Up Fake Environment ..."
+  package=$1
+  [[ $package ]] || {
+       log_error "No package name provided";
+       exit 1;
+  }
+  log_info "Preparing to install '$package' ..."
   for repo in $(ls $REPO_DIR/); do
     repo_path=$REPO_DIR/$repo
     [[ -d $repo_path ]] || continue
-    log_info "Browse repo ${repo} ..."
+    log_item "Browse repo ${repo} for '$package'..."
 
-    for package in $(ls $repo_path); do
-      package_path=$repo_path/$package
-      [[ -d $package_path ]] || continue
-      log_info "Setup package ${package} ..."
+    package_path=$repo_path/$package
+    [[ -d $package_path ]] || {
+         log_error;
+         continue;
+    }
+    log_ok
 
-      # Setup variables
-      PACKAGE=$package
-      DISTDIR=$PKG_DIR/$PACKAGE
-      WORKDIR=$TMP_DIR/$PACKAGE/work
-      DESTDIR=$TMP_DIR/$PACKAGE/image
-      ARCHIVE=()
+    # Setup variables
+    PACKAGE=$package
+    DISTDIR=$PKG_DIR/$PACKAGE
+    WORKDIR=$TMP_DIR/$PACKAGE/work
+    DESTDIR=$TMP_DIR/$PACKAGE/image
+    ARCHIVE=()
 
-      #include $CFG_DIR/${package}.conf
-      if ! include $package_path/${package}.setup; then
-        log_error "${package} skipped. The file ${package}.setup was not found."
-        break
-      fi
+    #include $CFG_DIR/${package}.conf
+    if ! include $package_path/${package}.setup; then
+      log_error "${package} skipped. The file ${package}.setup was not found."
+      break
+    fi
 
-      # create package directory
-      mkdir -p $DISTDIR
+    # create package directory
+    mkdir -p $DISTDIR
 
-      # fetch all needed files
-      if ! (
-        cd $DISTDIR
+    # fetch all needed files
+    if ! (
+      cd $DISTDIR
 
-        for idx in "${!FILES[@]}"; do
-          file=`basename ${FILES[$idx]}`
+      for idx in "${!FILES[@]}"; do
+        file=`basename ${FILES[$idx]}`
 
-          while true; do
+        while true; do
 
-            # Download file
-            while [ ! -f $file ]; do
-              log_item "Downloading ${FILES[$idx]} ..."
+          # Download file
+          while [ ! -f $file ]; do
+            log_item "Downloading ${FILES[$idx]} ..."
 
-              if fn_exists "fetch"; then
-                # custom fetch function
-                fetch
-              else
-                # default fetch
-                if ! wget ${FILES[$idx]}; then
-                  log_error "ERROR: Downloading ${file} failed."
-                  if confirm "Retry?"; then
-                    continue
-                  fi
-                else
-                  log_ok "Downloading ${file} finished successfully."
+            if fn_exists "fetch"; then
+              # custom fetch function
+              fetch
+            else
+              # default fetch
+              if ! wget ${FILES[$idx]}; then
+                log_error "ERROR: Downloading ${file} failed."
+                if confirm "Retry?"; then
+                  continue
                 fi
-              fi
-              break;
-            done
-
-            # Checking File
-            [ -f $file ] || return 1
-            log_item "Checking checksums ..."
-            if [[ -z $MD5[$idx] || -z $SHA1[$idx] ]]; then
-              log_warn "WARNING: md5 and sha1 should be provided to prevent using corrupted or manipulated files"
-            fi
-            if ! check-files $file ${MD5[$idx]} ${SHA1[$idx]}; then
-              if confirm "Retry downloading the file ${file}?"; then
-                rm $file
-                continue
               else
-                return 1
+                log_ok "Downloading ${file} finished successfully."
               fi
             fi
-            break
+            break;
           done
 
+          # Checking File
+          [ -f $file ] || return 1
+          log_item "Checking checksums ..."
+          if [[ -z $MD5[$idx] || -z $SHA1[$idx] ]]; then
+            log_warn "WARNING: md5 and sha1 should be provided to prevent using corrupted or manipulated files"
+          fi
+          if ! check-files $file ${MD5[$idx]} ${SHA1[$idx]}; then
+            return 1
+          fi
+          break
         done
-      ); then
-        log_error "ERROR: Not able to collect all needed files"
-        continue
-      fi
 
-      # create working directory
-      rm -rf $TMP_DIR/$PACKAGE
-      mkdir -p $WORKDIR
-      mkdir -p $DESTDIR
+      done
+    ); then
+      log_error "ERROR: Not able to collect all needed files"
+      continue
+    fi
 
-      # start setup process
-      (
-        cd $WORKDIR
-        for url in $FILES; do
-          file=`basename $url`;
-          ARCHIVE+=($DISTDIR/$file)
-        done
-        # start unpacking
+    # create working directory
+    rm -rf $TMP_DIR/$PACKAGE
+    mkdir -p $WORKDIR
+    mkdir -p $DESTDIR
 
-        env_begin "Unpacking fetched sources ..."
-        unpack && env_end || exit 1
+    # start setup process
+    (
+      cd $WORKDIR
+      for url in $FILES; do
+        file=`basename $url`;
+        ARCHIVE+=($DISTDIR/$file)
+      done
+      # start unpacking
 
-        env_begin "Setting up ${PACKAGE} ..."
-        setup && env_end || exit 1
+      env_begin "Unpacking fetched sources ..."
+      unpack && env_end || exit 1
 
-        env_begin "Install ${PACKAGE} ..."
-        cd $DESTDIR
-        cp -a . $ROOT_DIR && env_end || env_fail
-      )
-      env_begin "Removing temporary files ..."
-      # cleanup
-      cd $ROOT_DIR
-      rm -rf $TMP_DIR/$PACKAGE
-      env_end
-    done
+      env_begin "Setting up ${PACKAGE} ..."
+      setup && env_end || exit 1
+
+      env_begin "Install ${PACKAGE} ..."
+      cd $DESTDIR
+      cp -a . $CHNUM_HOME && env_end || env_fail
+    )
+    env_begin "Removing temporary files ..."
+    # cleanup
+    cd $CHNUM_HOME
+    rm -rf $TMP_DIR/$PACKAGE
+    env_end
   done
 }
 
